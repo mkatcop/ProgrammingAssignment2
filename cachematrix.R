dadcd14cf797a772d3471036f2e668ec3d4b491c
mkatcop <mitja@kostomaj.net>
2020-04-01 17:55:56 +0100

    Coden assigment

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,38 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  # 1. initialize the cache Matrix
+  # assign the value NULL 
+  
+  cacheM <- NULL
+  
+  # 2. define the method  'setM'
+  
+  setM <- function(y) {
+    x <<- y
+    cacheM <<- NULL
+  }
+  
+  # 3. define the method named 'getM' return the matrix 'x'
+  
+  getM <- function() x
+  
+  # 4. define the method named 'setC'
+  
+  setC <- function(inverse) cacheM <<- inverse
+  
+  # 5. define the method named 'getC' that returns the cached inverse of 'x'
+  
+  getC <- function() cacheM
+  
+  # 6. list the names of all methods 
+  list(setM = setM,
+       getM = getM,
+       setC = setC,
+       getC = getC)
+  
+  #---------------------------------------------------------
 }
 
 
@@ -12,4 +43,30 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  # 1. check the content of cache matrix
+  
+  cacheM <- x$getC()
+  
+  # 2. if the content is not null then: return the result 
+  
+  if (!is.null(cacheM)) {
+    message("loading cache matrix...")
+    return(cacheM)
+  }
+  
+  # 3. if the content is empty then: get the matrix, create, set, update and return the cache matrix
+  else {
+    dMatrix <- x$getM()
+    cacheM <- solve(dMatrix, ...)
+    x$setC(cacheM)
+    return(cacheM)
+  }
+  #---------------------------------------------------------
+  
 }
+
+My_Matrix <- makeCacheMatrix(matrix(0:8, 8, 8))
+My_Matrix$getM()
+My_Matrix$getC()
+cacheSolve(My_Matrix)
+My_Matrix$getC() 
